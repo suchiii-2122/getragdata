33940c58e6d756e5cd69833df797c0bf58721361
Suchitha <suchitha.ishwarakharvi@acuvate.com>
2026-02-09 16:46:54 +0530

    Initial commit

---
+++ query_search.py
@@ -0,0 +1,87 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+
+import os, logging
+from azure.search.documents import SearchClient # type: ignore
+from azure.search.documents.models import VectorizedQuery # type: ignore
+from azure.core.credentials import AzureKeyCredential
+from openai import AzureOpenAI
+
+# ---------------- CLIENTS ----------------
+openai_client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_KEY"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), # type: ignore
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION")
+)
+
+search_client = SearchClient(
+    endpoint=os.getenv("SEARCH_ENDPOINT"), # type: ignore
+    index_name=os.getenv("SEARCH_INDEX_NAME"), # type: ignore
+    credential=AzureKeyCredential(os.getenv("SEARCH_ADMIN_KEY")) # type: ignore
+)
+
+# ---------------- SEARCH ----------------
+class get_response:
+    @staticmethod
+    def search(query, top_k=5):
+        print(f"\n QUERY: {query}\n")
+
+        # Generate query embedding
+        q_vector = openai_client.embeddings.create(
+            model=os.getenv("AZURE_OPENAI_DEPLOYMENT"), # type: ignore
+            input=query
+        ).data[0].embedding
+
+        vector_query = VectorizedQuery(
+            vector=q_vector,
+            k_nearest_neighbors=top_k,
+            fields="text_vector"
+        )
+
+        results = search_client.search(
+        search_text=None,
+        vector_queries=[vector_query],
+            select=["chunk"]
+        )
+
+
+
+        final_results = []
+
+        for i, r in enumerate(results, start=1):
+            result_obj = {
+                "rank": i,
+                "score": round(r["@search.score"], 4),
+                # "sourceType": r.get("sourceType"),
+                # "sourceFile": r.get("sourceFile"),
+                "chunk": r.get("chunk"),
+            }
+
+            if r.get("imageUrl"):
+                result_obj["imageUrl"] = r["imageUrl"]
+
+            # Optional logging
+            logging.info({
+                "rank": i,
+                "score": result_obj["score"],
+                "preview": result_obj["chunk"][:300]
+            })
+
+            final_results.append(result_obj)
+
+        return final_results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
