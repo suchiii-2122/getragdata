7db9bbd90cc9bd9773248937d399ba9e7249c6e6
Suchitha <suchitha.ishwarakharvi@acuvate.com>
2026-02-09 18:45:57 +0530

    checking the lib azure.core and azure.search

--- function_app.py
+++ function_app.py
@@ -1,5 +1,7 @@
 import azure.functions as func
-import logging
+import logging, os, json
+from azure.search.documents import SearchClient
+from azure.core.credentials import AzureKeyCredential
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -15,11 +17,40 @@ def gitpushpublish(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully, now . Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+    try:
+        endpoint = os.getenv("SEARCH_ENDPOINT")
+        index_name = os.getenv("SEARCH_INDEX_NAME")
+        key = os.getenv("SEARCH_ADMIN_KEY")
+        if not all([endpoint,index_name,key]):
+            # raise ValueError
+            return func.HttpResponse(
+             " Failed to get all key points",
+             status_code=500
         )
+    except Exception as e:
+        logging.error(f'failed to get the endpoints')
+    try:
+        search_client = SearchClient(
+        endpoint=os.getenv("SEARCH_ENDPOINT"), # type: ignore
+        index_name=os.getenv("SEARCH_INDEX_NAME"), # type: ignore
+        credential=AzureKeyCredential(os.getenv("SEARCH_ADMIN_KEY")) # type: ignore
+    )
+        return func.HttpResponse(
+            json.dumps("successfully intialized the search client"),
+            status_code=200
+        )
+    except Exception as e:
+        logging.error(f'Failed to create search client: {e}')
+        return func.HttpResponse(
+             json.dumps(f'Failed to create search client: {e}'),
+             status_code=500
+        )
+
+
+    # if name:
+    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully, now . Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
